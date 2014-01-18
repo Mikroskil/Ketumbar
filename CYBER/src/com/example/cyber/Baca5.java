3a77f6a9215e54e60b29440cc46ef43848677eef
yudhawir4 <yudhawir4@gmail.com>
2014-01-19 03:10:10 +0700

    Baca.java

---
+++ CYBER/src/com/example/cyber/Baca5.java
@@ -0,0 +1,78 @@
+package com.example.cyber;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.ImageButton;
+
+public class Baca5 extends Activity {
+
+	MediaPlayer mp;
+    ImageButton one ;
+    
+	@Override
+	 protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.baca5);
+		ketikarrowback();
+		ketikarrowkiri();
+		ketikarrowkanan();
+		
+		
+		mp = MediaPlayer.create(this, R.raw.bola);
+        one = (ImageButton)this.findViewById(R.id.bola);
+
+        one.setOnClickListener(new View.OnClickListener() {
+
+           @Override
+           public void onClick(View v) {
+               
+               mp.start();
+           }
+       });
+		
+	}
+
+	protected void ketikarrowback(){
+		final Context context = this;
+		ImageButton arrow = (ImageButton) findViewById(R.id.arrowback); 
+		arrow.setOnClickListener(new View.OnClickListener() {  
+			   @Override  
+			   public void onClick(View b1) {  
+				   Intent intent = new Intent(context,Membaca.class);
+					startActivity(intent);
+			   }  
+			  });
+		
+		}
+	
+	
+	protected void ketikarrowkiri(){
+		final Context context = this;
+		ImageButton btn1 = (ImageButton)findViewById(R.id.leftarrow);
+		btn1.setOnClickListener(new OnClickListener(){
+			public void onClick (View a){
+				Intent intent = new Intent(context,Baca4.class);
+				startActivity(intent);
+			}
+		});
+	}
+	
+	protected void ketikarrowkanan(){
+		final Context context = this;
+		ImageButton btn2 = (ImageButton)findViewById(R.id.rightarrow);
+		btn2.setOnClickListener(new OnClickListener(){
+			public void onClick (View a2){
+				Intent intent = new Intent(context,Baca6.class);
+				startActivity(intent);
+			}
+		});
+	}
+
+}
