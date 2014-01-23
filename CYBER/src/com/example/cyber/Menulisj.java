18387171507ad08e51b7fa08d7ce57499bb77b67
yudhawir4 <yudhawir4@gmail.com>
2014-01-23 16:30:11 +0700

    menulisA-M.java

---
+++ CYBER/src/com/example/cyber/Menulisj.java
@@ -0,0 +1,64 @@
+package com.example.cyber;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.ImageButton;
+
+public class Menulisj extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.menulisj);
+		
+		ketikarrowback();
+		ketikarrowkanan();
+		ketikarrowkiri();
+	}
+
+	protected void ketikarrowback(){
+		final Context context = this;
+		ImageButton arrow = (ImageButton) findViewById(R.id.arrowback); 
+		arrow.setOnClickListener(new View.OnClickListener() {  
+			   @Override  
+			   public void onClick(View baca) {  
+				   Intent intent = new Intent(context,AppActivity.class);
+					startActivity(intent);
+			   }  
+			  });
+		
+		}
+	
+	protected void ketikarrowkiri(){
+		final Context context = this;
+		ImageButton btn1 = (ImageButton)findViewById(R.id.leftarrow);
+		btn1.setOnClickListener(new OnClickListener(){
+			public void onClick (View a){
+				Intent intent = new Intent(context,Menulisi.class);
+				startActivity(intent);
+			}
+		});
+	}
+	
+	protected void ketikarrowkanan(){
+		final Context context = this;
+		ImageButton btn2 = (ImageButton)findViewById(R.id.rightarrow);
+		btn2.setOnClickListener(new OnClickListener(){
+			public void onClick (View a2){
+				Intent intent = new Intent(context,Menulisk.class);
+				startActivity(intent);
+			}
+		});
+	}
+	
+
+}
+
+
+
